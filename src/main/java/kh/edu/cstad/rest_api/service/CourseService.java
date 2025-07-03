497020ad7b44ca1e0b040b9283c2ed8ed3ee0a00
Sam-Sokunsreypich <sokunsreypichsam@gmail.com>
2025-07-03 19:05:07 +0700

    add: Doc. Comment

--- src/main/java/kh/edu/cstad/rest_api/service/CourseService.java
+++ src/main/java/kh/edu/cstad/rest_api/service/CourseService.java
@@ -7,15 +7,30 @@ import java.util.List;
 
 public interface CourseService {
     /**
-     * For get all course
+     * For get all courses
      * @author SreyPich
      * @return List<CourseResponse>
      */
     List<CourseResponse> getAllCourses(Boolean status);
 
+    /**
+     * For get courses By Status and By Title
+     * @author SreyPich
+     * @return List<CourseResponse>
+     */
     List<CourseResponse> getCourses(Boolean status,String title);
 
+    /**
+     * For get course By Code
+     * @author SreyPich
+     * @return CourseResponse
+     */
     CourseResponse getCourseByCode(String code);
 
+    /**
+     * For get course By Id
+     * @author SreyPich
+     * @return CourseResponse
+     */
     CourseResponse getCourseById(String id);
 }
